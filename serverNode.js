c73aff5aa18203584684b7c7f6710f96916bd6cc
RK <rehane.karadjia@icloud.com>
2018-06-21 12:19:39 +0200

    creation d'une route recuperant une seule ligne dans la BDD

--- serverNode.js
+++ serverNode.js
@@ -40,6 +40,23 @@ app.listen(3000,()=>console.log('server is running port 3000'))
   });
 
 
+  app.get('/trajets/:id',(res,req)=>{ //definition de la route    localhost:3000/trajets
+    console.log('id dans url ' + res.params.id);
+
+    con.getConnection(function (err, connection) {
+      // Use the connection
+     // "SELECT RAP_BILAN FROM RAPPORT_VISITE where RAP_NUM = '"+numRapport+"' "
+      connection.query("SELECT * FROM trajets WHERE trajets.id = '"+res.params.id+"'", (err,rows)=> {
+        if (err) throw err;
+       console.log(rows);return rows;
+      });
+      //met fin à la connection 
+      connection.release();
+  });
+ 
+});
+
+
 //}
 
 
